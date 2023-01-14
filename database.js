187beda09190c70c7ce08cba65fa268046659fdf
Anthonia Nwanya <tonianwanya@gmail.com>
2023-01-15 00:07:43 +0100

    Created database

---
+++ database.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+mongoose.set('strictQuery', true);
+
+function mongooseDB(){
+ mongoose.connect(process.env.MONGO_URI)
+
+ mongoose.connection.on("connected", () => {
+console.log("Connected to MongoDB Successfully!")
+ })
+
+ mongoose.connection.on("error", (err) => {
+    console.log("An error occurred")
+    console.log(err)
+    
+ })
+}
+
+module.exports = { mongooseDB }
